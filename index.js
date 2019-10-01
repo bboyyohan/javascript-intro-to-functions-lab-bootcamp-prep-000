b3933872116afc60c5c83836c527b02660b3ebd3
stupendous-patch-9345 <yo.park93@yahoo.com>
2019-10-01 16:01:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ function logShout(string) {
  console.log(`HELLO`);
 }
 
-function logWshiper(string) {
+function logWhisper(string) {
   consoloe.log(`hello`);
 }
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) {
+    return `I can't hear you!`
+  } else if (string === string.toUpperCase()) {
+    return `YES INDEED!`
+  } else if (string === `I love you, Grandma.`) {
+    return `I love you, too.`
+  }
+}
+  
